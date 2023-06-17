adf2bc081e581447457d2a6e72e25bd9491f7d06
Dan-Koch <135790458+Dan-Koch@users.noreply.github.com>
2023-06-16 21:29:29 -0700

    Add files via upload

--- App.py
+++ App.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 #importing any libraries we may need
 import pandas as pd
 import plotly_express as px
 import streamlit as st
 
-
-# In[2]:
 #importing dataframe
-vehicles_us = pd.read_csv('/vehicles_us.csv')
+vehicles_us = pd.read_csv('vehicles_us.csv')
 
 #filling paint color with unknown
 vehicles_us['paint_color'].fillna('unknown', inplace= True)
@@ -31,18 +26,15 @@ vehicles_us['odometer'].fillna(odometer_median, inplace= True)
 
 #filling NaN values with 0 for is_4wd
 vehicles_us['is_4wd'].fillna(0, inplace= True)
-# In[3]:
+
 #creating a header with streamlit
 st.header('Information on Cars in the US')
 st.dataframe(vehicles_us)
 
-# In[4]:
 #creating a selectbox
 model_veh = vehicles_us['model'].unique()
 vehicle_select = st.selectbox('model:', model_veh)
 
-
-# In[5]:
 #creating a slider for the model year
 #variables for the min and max of the slider
 min_year= int(vehicles_us['model_year'].min())
@@ -52,8 +44,6 @@ max_year= int(vehicles_us['model_year'].max())
 year_range = st.slider("Choose years", value= (min_year, max_year), min_value= min_year, max_value= max_year)
 real_range = list(range(year_range[0], year_range[1]+ 1))
 
-
-# In[6]:
 #filtering dataframe based on selectbox and year range
 filter_model_year = vehicles_us[(vehicles_us['model'] == vehicle_select) & (vehicles_us['model_year'].isin(list(real_range)))]
 
@@ -61,8 +51,6 @@ filter_model_year = vehicles_us[(vehicles_us['model'] == vehicle_select) & (vehi
 st.write("Here you see all the vehicles of the model you selected that was built within the years on the slider.")
 st.table(filter_model_year)
 
-
-# In[7]:
 #creating a list for historgram
 list_for_hist= ['odometer', 'fuel', 'type', 'condition',]
 
@@ -77,8 +65,6 @@ graph1.update_layout(title= "<b>Split of Price by {}</b>".format(hist_select))
 
 st.plotly_chart(graph1)
 
-
-# In[9]:
 #creating a scatter plot of price vs a one column from a list
 #creating a list to choose from
 list_for_scatter = ['odometer', 'days_listed', 'cylinders', 'condition']
@@ -91,9 +77,7 @@ graph2= px.scatter(vehicles_us, x= 'price', y= scatter_select, hover_data=['mode
 graph2.update_layout(title="<b>Price vs {}</b> ".format(scatter_select))
 st.plotly_chart(graph2)
 
-
-# In[ ]:
-# #creatting a scatter plot of days listed vs a few columns from a list
+#creatting a scatter plot of days listed vs a few columns from a list
 list_for_hist_days= ['price', 'condition', 'transmission', 'fuel', 'model_year']
 hist_select_days= st.selectbox('Select a column to see how many days it is listed', list_for_hist_days)
 
